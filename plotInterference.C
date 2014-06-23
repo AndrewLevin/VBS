e84138cf8b08956df8195ae0fee4850ddd5879eb
Andrew <anlevin@cern.ch>
2014-06-23 16:19:30 +0200

    no commit message

--- plotInterference.C
+++ plotInterference.C
@@ -1,3 +1,5 @@
+//this should go in the directory of https://github.com/AndrewLevin/WWScattering
+//change the name of the .rootlogon.C file if this macro does not work at first
 
 #include "TROOT.h"
 #include "TSystem.h"
@@ -19,16 +21,16 @@ void plotInterference(){
 
   gStyle->SetFillStyle(0);
   gStyle->SetLegendBorderSize(0); 
-gROOT->ForceStyle();
+  gROOT->ForceStyle();
 
 
-  TFile* file_ewk = new TFile("histo_nice8TeV_ewk_wz.root", "read");
-  TFile* file_qcd = new TFile("histo_nice8TeV_qcd_wz.root", "read");
-  TFile* file_ewk_plus_qcd = new TFile("histo_nice8TeV_ewk_plus_qcd_wz.root", "read");
+  TFile* file_ewk = new TFile("/data/blue/anlevin/VBS/21Jun2014/wwss_mjj_qed_4_qcd_0_sm.root", "read");
+  TFile* file_qcd = new TFile("/data/blue/anlevin/VBS/21Jun2014/wwss_mjj_qed_2_qcd_99_sm.root", "read");
+  TFile* file_ewk_plus_qcd = new TFile("/data/blue/anlevin/VBS/21Jun2014/wwss_mjj_qed_4_qcd_99_sm.root", "read");
 
-  TH1F* hSignalEwk = (TH1F*)file_ewk->Get("histo1");
-  TH1F* hSignalQCD = (TH1F*)file_qcd->Get("histo1");
-  TH1F* hSignalEwkPlusQCD = (TH1F*)file_ewk_plus_qcd->Get("histo1");
+  TH1F* hSignalEwk = (TH1F*)file_ewk->Get("histo0");
+  TH1F* hSignalQCD = (TH1F*)file_qcd->Get("histo0");
+  TH1F* hSignalEwkPlusQCD = (TH1F*)file_ewk_plus_qcd->Get("histo0");
 
   //TCanvas* c2 = new TCanvas("c2", "c2",700,50,500,500);
   //c2->cd(1);
@@ -36,9 +38,15 @@ gROOT->ForceStyle();
   TH1F * hSum= (TH1F*)hSignalEwk->Clone();
   hSum->Add(hSignalQCD);
 
+  hSum->Draw();
+
+
+
   TLegend* legend = new TLegend(0.5,0.74,0.8,0.9);
   legend->AddEntry(hSum,"sum of EWK and QCD");
   legend->AddEntry(hSignalEwkPlusQCD,"EWK and QCD with interference");
+
+
 
   legend->SetBorderSize(     0);                                                                                                                            
   legend->SetFillColor (     0);                                                                                                                            
@@ -46,17 +54,26 @@ gROOT->ForceStyle();
   legend->SetTextFont  (    42);                                                                                                                            
   legend->SetTextSize(0.03);
 
+
+
   hSum->SetLineWidth(3);
   hSignalEwkPlusQCD->SetLineWidth(3);
   hSum->SetStats(0);
 
+
+
   hSum->SetTitle("");
   hSum->SetLineColor(kRed);
 
+
+
   hSum->Draw("e");
-  hSum->GetYaxis()->SetRangeUser(0,1);  
+  hSum->GetYaxis()->SetRangeUser(0,4);  
   hSum->GetXaxis()->SetTitle("m_{jj} (GeV)}");
+
+
 
   hSignalEwkPlusQCD->Draw("eSAME");
   legend->Draw("SAME");
+
 }
