b09bb98418b2542ffb2031f9f7d8c11f566b1d3a
Andrew <anlevin@cern.ch>
2014-09-14 11:05:13 +0200

    no commit message

--- 13_tev/select_events.py
+++ 13_tev/select_events.py
@@ -5,20 +5,33 @@
 import optparse
 from ROOT import *
 import sys
+from array import array
 
-gStyle.SetOptStat(0)
+#gStyle.SetOptStat(0)
 
 def fillHistograms(t,hist):
     for entry in range(t.GetEntries()):
         t.GetEntry(entry)
 
-        if t.lep1q == t.lep2q:
+        if (abs(t.lep1id) == 13 and abs(t.lep2id) == 11) or (abs(t.lep1id) == 11 and abs(t.lep1id) == 13) :
+            channel="em"
+        elif abs(t.lep1id) == 13 and abs(t.lep2id) == 13:
+            channel = "mm"
+        elif abs(t.lep1id) == 11 and abs(t.lep2id) == 11:
+            channel = "ee"
+        else:
+            assert(0)
+
+        if channel != "em":
             continue
 
-        #if (t.jet1+t.jet2).M() < 500:
-        #    continue
-        #if abs(t.jet1.Eta() - t.jet2.Eta()) < 2.5:
-        #    continue
+        if t.lep1q != t.lep2q:
+            continue
+
+        if (t.jet1+t.jet2).M() < 500:
+            continue
+        if abs(t.jet1.Eta() - t.jet2.Eta()) < 2.5:
+            continue
 
         lep1passfullid = bool(t.cuts & 1<<2)
         lep2passfullid = bool(t.cuts & 1<<9)
@@ -29,18 +42,26 @@ def fillHistograms(t,hist):
         if not lep2passfullid:
             continue
 
+        w=t.xsWeight*float(options.lumi)
+
         if options.variable == "mjj":
-            hist.Fill((t.jet1+t.jet2).M())
+            #put the overflow in the last bin
+            if (t.jet1+t.jet2).M() > hist.GetBinLowEdge(hist.GetNbinsX()):
+                hist.Fill(hist.GetBinCenter(hist.GetNbinsX()),w)
+            else:              
+                hist.Fill((t.jet1+t.jet2).M(),w)
         elif options.variable == "mll":    
-            hist.Fill((t.lep1+t.lep2).M())
+            hist.Fill((t.lep1+t.lep2).M(),w)
         elif options.variable == "met":    
-            hist.Fill(t.metpt)
+            hist.Fill(t.metpt,w)
         elif options.variable == "detajj":
-            hist.Fill(abs(t.jet1.Eta() - t.jet2.Eta()))
+            hist.Fill(abs(t.jet1.Eta() - t.jet2.Eta()),w)
         elif options.variable == "jet1btag":
-            hist.Fill(t.jet1btag)    
+            hist.Fill(t.jet1btag,w)    
         elif options.variable == "jet2btag":            
-            hist.Fill(t.jet2btag)    
+            hist.Fill(t.jet2btag,w)
+        elif options.variable == "nvtx":            
+            hist.Fill(t.nvtx,w)                
         else:
             assert(0)
         
@@ -48,14 +69,23 @@ def fillHistograms(t,hist):
 parser = optparse.OptionParser()
 
 parser.add_option('-v', '--variable', help='which variable to plot', dest='variable', default='mjj')
+parser.add_option('-c', '--datacard', help='the name of the file to write the datacard to', dest='datacard', default='datacard.txt')
+parser.add_option('-l', '--lumi', help='the amount of integrated luminosity to weight the events with', dest='lumi', default='19.4')
+parser.add_option('-o', '--output_dir', help='the directory to write the output plots', dest='output_dir', default='/afs/cern.ch/user/a/anlevin/www/tmp/')
 
 (options,args) = parser.parse_args()
 
 
+signal_fname="delete_this_2.root"
+background_fname="delete_this_2.root"
+
+#signal_fname="/afs/cern.ch/work/a/anlevin/crab/CMSSW_7_0_6_patch3/src/output_tree.root"
+#background_fname="/afs/cern.ch/work/a/anlevin/crab/CMSSW_7_0_6_patch3/src/output_tree.root"
+
 #signal_fname="/data/blue/anlevin/ntuples/wpwp_13_tev_qed_4_qcd_0_v3.root"
-signal_fname="/data/blue/anlevin/ntuples/ttbar_normal_mixing.root"
+#signal_fname="/data/blue/anlevin/ntuples/ttbar_normal_mixing.root"
 #background_fname="/data/blue/anlevin/ntuples/wpwp_13_tev_qed_4_qcd_0_v3.root"
-background_fname="/data/blue/anlevin/ntuples/ttbar_pre_mixing.root"
+#background_fname="/data/blue/anlevin/ntuples/ttbar_pre_mixing.root"
 #background_fname="/afs/cern.ch/user/a/anlevin/merged.root"
 
 c=TCanvas("c", "c",0,0,600,500)
@@ -64,11 +94,13 @@ c.Range(0,0,1,1)
 f_signal=TFile(signal_fname)
 f_background=TFile(background_fname)
 
-tree_signal=f_signal.Get("demo/events")
-tree_background=f_background.Get("demo/events")
+tree_signal=f_signal.Get("events")
+tree_background=f_background.Get("events")
 
 if options.variable == "mjj":
-    hist = TH1F('mjj', 'mjj', 35, 0., 2000 )
+    binning=array('f',[500,700,1100,1600,2000])
+    hist = TH1F('mjj', 'mjj',4, binning )
+    #hist = TH1F('mjj', 'mjj', 35, 0., 2000 )
 elif options.variable == "mll":    
     hist = TH1F('mll', 'mll', 35, 0., 500)
 elif options.variable == "met":
@@ -76,11 +108,16 @@ elif options.variable == "met":
 elif options.variable == "detajj":
     hist = TH1F('detajj', 'detajj', 35, 0., 5 )
 elif options.variable == "jet1btag":
-    hist = TH1F('detajj', 'jet1btag', 35, -1., 1 )
+    hist = TH1F('jet1btag', 'jet1btag', 35, -1., 1 )
 elif options.variable == "jet2btag":
-    hist = TH1F('detajj', 'jet2btag', 35, -1., 1 )        
+    hist = TH1F('jet2btag', 'jet2btag', 35, -1., 1 )
+elif options.variable == "nvtx":
+    hist = TH1F('nvtx', 'nvtx', 35, 0., 60 )        
 else:
     assert(0)
+
+#error = sqrt(sum weight^2)
+hist.Sumw2()
 
 hist_signal=hist.Clone()
 hist_background=hist.Clone()
@@ -100,11 +137,8 @@ hist_background.GetXaxis().SetLabelSize(0.0)
 fillHistograms(tree_signal,hist_signal)
 fillHistograms(tree_background,hist_background)
 
-hist_signal.Sumw2()
-hist_background.Sumw2()
-
-hist_signal.Scale(1/hist_signal.Integral())
-hist_background.Scale(1/hist_background.Integral())
+#hist_signal.Scale(1/hist_signal.Integral())
+#hist_background.Scale(1/hist_background.Integral())
 
 hist_signal.SetLineColor(kRed)
 hist_background.SetLineColor(kBlue)
@@ -160,7 +194,19 @@ pbottom.Update()
 ptop.Update()
 c.Update()
 
-c.SaveAs("/afs/cern.ch/user/a/anlevin/www/tmp/"+options.variable+".png")
+c.SaveAs(options.output_dir+options.variable+".png")
+
+dcard = open(options.datacard,'w')
+
+print >> dcard, "imax 1 number of channels"
+print >> dcard, "jmax * number of background"
+print >> dcard, "kmax * number of nuisance parameters"
+print >> dcard, "Observation 0"
+print >> dcard, "bin bin1 bin1"
+print >> dcard, "process WWjj ttbar"
+print >> dcard, "process 0 1"
+print >> dcard, "rate "+str(hist_signal.Integral())+" 0.1"
+print >> dcard, "lumi_8tev lnN 2.4 2.4"
 
 raw_input()
 
