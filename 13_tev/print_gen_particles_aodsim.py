cd2cab87d5bd712bafb03192f31e7b1612dac98e
Andrew <anlevin@cern.ch>
2015-03-31 10:25:43 +0200

    no commit message

--- 13_tev/print_gen_particles_aodsim.py
+++ 13_tev/print_gen_particles_aodsim.py
@@ -9,7 +9,21 @@ def deltaR(a,b):
     if dphi > pi: dphi = 2*pi-dphi
     return hypot(a.eta()-b.eta(),dphi)
 
-events = Events (['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/QCD_HT-100To250_13TeV-madgraph/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/7ADC540A-BA86-E411-9B8F-002590747DEC.root'])
+#events = Events (['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/QCD_HT-100To250_13TeV-madgraph/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/7ADC540A-BA86-E411-9B8F-002590747DEC.root'])
+
+#events = Events (['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/QCD_HT-100To250_13TeV-madgraph/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/F61CFA31-B886-E411-935B-0025907B50D2.root'])
+
+#events = Events( ['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/D0E76C6C-EB74-E411-97B7-0025907FD2DA.root'])
+
+events = Events (['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/QCD_Pt-30to50_Tune4C_13TeV_pythia8/AODSIM/PU20bx25_trkalmb_castor_PHYS14_25_V1-v1/00000/A4018A72-C273-E411-82BF-002590200AE4.root'])
+
+#events = Events ( ['file:eos/cms/store/user/anlevin/data/AOD/wpwp_13_tev_qed_4_qcd_0/step4_output_801.root']  )
+
+#events = Events( ['root://cms-xrd-global.cern.ch//store/user/anlevin/data/MINIAOD/sherpa_SM_v2/step5_output_9701.root'])
+
+#events = Events( ['file:/afs/cern.ch/work/a/anlevin/tmp/sherpa_MYTEST_SM_MASTER_cff_py_GEN.root'] )
+
+#events = Events (['root://cms-xrd-global.cern.ch//store/mc/Phys14DR/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/AODSIM/PU20bx25_PHYS14_25_V1-v1/00000/000470E0-3B75-E411-8B90-00266CFFA604.root'])
 
 muons, muonLabel = Handle("std::vector<reco::Muon>"), "muons"
 genparticles, genParticlesLabel = Handle("vector<reco::GenParticle>"), "genParticles"
@@ -22,37 +36,29 @@ genparticles, genParticlesLabel = Handle("vector<reco::GenParticle>"), "genParti
 count= 0
 for event in events:
 
-    if event.eventAuxiliary().luminosityBlock() != 48598:
+    #if event.eventAuxiliary().luminosityBlock() != 48598:
+    #    continue
+
+    if event.eventAuxiliary().event() != 1195952 :
         continue
 
-    if event.eventAuxiliary().event() != 4814521:
-        continue
-
-    print "run %6d, lumi %4d, event %12d" % (event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
+    #print "run %6d, lumi %4d, event %12d" % (event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
     
-    event.getByLabel(muonLabel, muons)
     event.getByLabel(genParticlesLabel, genparticles)
 
-    mu=muons.product()[1]
-
-    #print mu.isLooseMuon()
-    print mu.isGlobalMuon()
-    print mu.isTrackerMuon()
-    print mu.isStandAloneMuon()
-    print mu.pt()
-    print mu.innerTrack().pt()
-    print mu.outerTrack().pt()
-    print mu.globalTrack().pt()
-    print mu.globalTrack().normalizedChi2()
-
-    #mu.
-    
-    #for i,mu in enumerate(muons.product()): 
-    #	print "pt : "+str(mu.pt())+ " eta : " +str(mu.eta()) + " phi : " + str(mu.phi())
+    #for genparticle in genparticles.product():
+    #    if abs(genparticle.pdgId()) == 15:
+    #        for i in range(0,genparticle.numberOfDaughters()):
+    #            print str(genparticle.pdgId())+" "+str(genparticle.status())+" "+str(genparticle.daughter(i).pdgId())
 
     for genparticle in genparticles.product():
-        if genparticle.pt() < 5:
-            continue
+        if abs(genparticle.pdgId()) == 13:
+            print "found muon"
+        if genparticle.mother(0) and genparticle.mother(1):
+            print str(genparticle.pdgId())+" "+str(genparticle.status())+" "+str(genparticle.pt())+" "+str(genparticle.mother(0).pdgId())+" "+str(genparticle.mother(0).pt())+" "+str(genparticle.mother(1).pdgId())+" "+str(genparticle.mother(1).pt())+" "+str(genparticle.numberOfMothers())
+        elif genparticle.mother(0):
+            print str(genparticle.pdgId())+" "+str(genparticle.status())+" "+str(genparticle.pt())+" "+str(genparticle.mother(0).pdgId())+" "+str(genparticle.mother(0).pt())+" "+str(genparticle.numberOfMothers())
+        else:
+            print str(genparticle.pdgId())+" "+str(genparticle.status())+" "+str(genparticle.pt())+" "+str(genparticle.numberOfMothers())
 
-        if deltaR(genparticle,mu) < 1.0:
-	    print str(deltaR(genparticle,mu))+" "+str(genparticle.pt())+" "+str(genparticle.pdgId())
+    break     
