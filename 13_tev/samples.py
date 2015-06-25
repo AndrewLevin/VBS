c00b70ebf54254983aaa21baab3c9464285b854f
Andrew <anlevin@cern.ch>
2015-06-25 20:03:50 +0200

    no commit message

---
+++ 13_tev/samples.py
@@ -0,0 +1,22 @@
+samples = {
+    "wpwpjj-ewk" :
+    1 << 1,
+    "wpwpjj-qcd" :
+    1 << 2,
+    "wpwpjj-ewk-qcd" :
+    1 << 3,
+    "ttbar" :
+    1 << 4,
+    "qcd" :
+    1 << 5,
+    "wjets" :
+    1 << 6,
+    "zjets" :
+    1 << 7,
+    "wz" :
+    1 << 8,
+    "wpwmjj" :
+    1 << 9,
+    "gm" :
+    1 << 10,        
+} 
