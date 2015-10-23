48ae49e057cdc927f606310c851e9ef0bb17d57a
Andrew <anlevin@cern.ch>
2015-10-23 14:31:18 +0200

    have a separate selection without the lepton ids

--- 13_tev/selection.py
+++ 13_tev/selection.py
@@ -2,7 +2,34 @@ from ROOT import *
 
 gROOT.ProcessLine('#include "/afs/cern.ch/work/a/anlevin/cmssw/CMSSW_7_4_1_patch1/src/ntuple_maker/ntuple_maker/interface/enum_definition.h"')
 
-def passSelection(t):
+def passSelection(t,mode):
+
+    p=True
+
+    if not passSelectionExceptLeptonIDs(t,mode):
+        p = False
+
+    lep1passfullid = bool(t.flags & Lep1TightSelectionV1)
+    lep2passfullid = bool(t.flags & Lep2TightSelectionV1)
+
+    if not lep1passfullid:
+        p=False
+
+    if not lep2passfullid:
+        p=False
+
+    if (t.flags & WLLJJVetoV1):
+        p=False
+
+    if (t.flags & WLLJJVetoV2):
+        p=False
+
+    #if (t.flags & WLLJJVetoV6):
+    #    p=False                
+
+    return p
+
+def passSelectionExceptLeptonIDs(t,mode):
 
     p=True
 
@@ -15,22 +42,19 @@ def passSelection(t):
     if t.lep1q != t.lep2q:
         p=False
 
-    if (t.jet1+t.jet2).M() < 500:
+    if abs(t.jet1.Eta()) > 2.5 or abs(t.jet2.Eta()) > 2.5:
         p=False
 
-    if abs(t.jet1.Eta() - t.jet2.Eta()) < 2.5:
-        p=False
+    if mode == "sm_low_mjj_control_region":    
+        if (t.jet1+t.jet2).M() > 500:
+            p=False
+    else:        
+        if (t.jet1+t.jet2).M() < 500:
+            p=False
+        if abs(t.jet1.Eta() - t.jet2.Eta()) < 2.5:
+            p=False
 
     if t.maxbtagevent > 0.89:
-        p=False
-
-    lep1passfullid = bool(t.flags & Lep1TightSelectionV1)
-    lep2passfullid = bool(t.flags & Lep2TightSelectionV1)
-
-    if not lep1passfullid:
-        p=False
-
-    if not lep2passfullid:
         p=False
 
     return p
