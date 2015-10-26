7b7998410941ef2e712cd9605e29eeaa6996c629
Andrew <anlevin@cern.ch>
2015-10-26 13:59:09 +0100

    check the fraction of wzjj events with leptons inside the acceptance

---
+++ 13_tev/miniaodsim_gen_particle_analyzer_1.py
@@ -0,0 +1,81 @@
+import ROOT
+import sys
+from DataFormats.FWLite import Events, Handle
+from math import *
+
+def isAncestor(a,p) :
+        if a == p : 
+                return True
+        for i in xrange(0,p.numberOfMothers()) :
+                if isAncestor(a,p.mother(i)) :
+                         return True
+        return False
+
+
+
+#events = Events('file:/afs/cern.ch/work/a/anlevin/VBS/13_tev/Merged.root')
+#events = Events('file:/afs/cern.ch/work/a/anlevin/tmp/MySkim_1.electrons.root')
+
+#events = Events (['root://cms-xrd-global.cern.ch//store/mc/RunIISpring15DR74/WLLJJToLNu_M-60_EWK_QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/60000/7EB991EF-8566-E511-938E-0025905C2CBE.root'])
+
+#events = Events (['root://eoscms.cern.ch//eos/cms/store/mc/RunIISpring15DR74/WLLJJToLNu_M-60_EWK_QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/50000/102AAAF1-0966-E511-B871-24BE05C6D5B1.root'])
+
+events = Events ( ['root://eoscms.cern.ch//eos/cms/store/mc/RunIISpring15DR74/WLLJJToLNu_M-60_EWK_QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/50000/807E47CC-0966-E511-A480-B083FED00118.root']) 
+
+#events = Events (['root://eoscms.cern.ch//eos/cms/store/mc/RunIISpring15DR74/WLLJJToLNu_M-4to60_EWK_QCD_TuneCUETP8M1_13TeV-madgraph-pythia8/MINIAODSIM/Asympt25ns_MCRUN2_74_V9-v1/30000/BC441FED-7662-E511-A233-C8600032C755.root'])
+
+lheinfo,lheinfoLabel = Handle("LHEEventProduct"), "externalLHEProducer"
+
+muons, muonLabel = Handle("std::vector<pat::Muon>"), "slimmedMuons"
+handlePruned  = Handle ("std::vector<reco::GenParticle>")
+handlePacked  = Handle ("std::vector<pat::PackedGenParticle>")
+
+labelPruned = ("prunedGenParticles")
+labelPacked = ("packedGenParticles")
+
+n_events_run_over = 0
+
+n_events_with_three_leptons_in_acceptance = 0
+
+n_events_with_three_leptons = 0
+
+for event in events:
+
+    if n_events_run_over % 1000 == 0:
+        print n_events_run_over
+
+    n_events_run_over=n_events_run_over+1
+
+    #print "run %6d, lumi %4d, event %12d" % (event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
+    
+    event.getByLabel(muonLabel, muons)
+    
+    event.getByLabel(lheinfoLabel,lheinfo)
+
+    n_leptons_in_acceptance = 0
+    n_leptons = 0
+
+    for i in range(0,len(lheinfo.product().hepeup().IDUP)):
+	v=ROOT.TLorentzVector();
+        v.SetPxPyPzE(lheinfo.product().hepeup().PUP.at(i)[0],lheinfo.product().hepeup().PUP.at(i)[1],lheinfo.product().hepeup().PUP.at(i)[2],lheinfo.product().hepeup().PUP.at(i)[3])
+
+	if v.Pt() > 10 and abs(v.Eta()) < 2.5 and (abs(lheinfo.product().hepeup().IDUP.at(i)) == 11 or abs(lheinfo.product().hepeup().IDUP.at(i)) == 13):
+            n_leptons_in_acceptance=n_leptons_in_acceptance+1
+
+	if abs(lheinfo.product().hepeup().IDUP.at(i)) == 11 or abs(lheinfo.product().hepeup().IDUP.at(i)) == 13:
+            n_leptons=n_leptons+1
+
+    assert(n_leptons_in_acceptance <= 3)
+    assert(n_leptons <= 3)
+
+    if n_leptons_in_acceptance == 3: 
+        n_events_with_three_leptons_in_acceptance = n_events_with_three_leptons_in_acceptance+1   
+
+    if n_leptons == 3:
+        n_events_with_three_leptons = n_events_with_three_leptons+1
+
+print "n_events_with_three_leptons = "+str(n_events_with_three_leptons)
+print "n_events_with_three_leptons_in_acceptance = "+str(n_events_with_three_leptons_in_acceptance)
+print "n_events_run_over = " +str(n_events_run_over)
+print "float(n_events_with_three_leptons_in_acceptance)/float(n_events_run_over) = "+ str(float(n_events_with_three_leptons_in_acceptance)/float(n_events_run_over))
+print "float(n_events_with_three_leptons)/float(n_events_run_over) = "+ str(float(n_events_with_three_leptons)/float(n_events_run_over))
