f59f1ff369e081ceedd06b935e4f38b7ed458c2d
Andrew <anlevin@cern.ch>
2015-11-14 15:49:21 +0100

    add script to merge flattish ntuples at CERN on eos

---
+++ 13_tev/merge_flattish_ntuples.sh
@@ -0,0 +1,31 @@
+input_dir=eos/cms/store/user/anlevin/DoubleMuon/flattish_ntuples_v213
+
+filelist=""; 
+
+for dir1 in `/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls ${input_dir}`; 
+
+do 
+
+echo $dir1
+
+for dir2 in `/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls ${input_dir}/${dir1}`; 
+
+do
+
+for file in `/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select ls ${input_dir}/${dir1}/${dir2} | grep -v failed`; 
+
+do 
+
+#echo $file
+
+filelist=$filelist" "root://eoscms//${input_dir}/${dir1}/${dir2}/$file; 
+
+done; 
+
+done;
+
+done;
+
+echo $filelist; 
+
+hadd output_tree.root $filelist;
