f395beb6cd0565b2d3546f5410707dc084efe5d4
Andrew <anlevin@cern.ch>
2015-12-15 12:28:14 +0100

    also print out muons

--- 13_tev/miniaodsim_analyzer_2.py
+++ 13_tev/miniaodsim_analyzer_2.py
@@ -4,10 +4,11 @@ import ROOT
 import sys
 from DataFormats.FWLite import Events, Handle
 
-events = Events (['root://eoscms.cern.ch//eos/cms/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/60000/08769053-AF70-E511-A7A1-003048FFD730.root'])
+events = Events (['root://eoscms.cern.ch//eos/cms/store/mc/RunIISpring15MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/74X_mcRun2_asymptotic_v2-v1/50000/ACE7C21B-6E6F-E511-8C2F-001E67504D2D.root'])
 
 lheinfo,lheinfoLabel = Handle("LHEEventProduct"), "externalLHEProducer"
 
+muons, muonLabel = Handle("std::vector<pat::Muon>"), "slimmedMuons"
 electrons, electronLabel = Handle("std::vector<pat::Electron>"), "slimmedElectrons"
 handlePruned  = Handle ("std::vector<reco::GenParticle>")
 handlePacked  = Handle ("std::vector<pat::PackedGenParticle>")
@@ -20,15 +21,16 @@ count= 0
 
 for event in events:
 
-    if event.eventAuxiliary().luminosityBlock() != 143886:
+    if event.eventAuxiliary().luminosityBlock() != 209529:
         continue
 
-    if event.eventAuxiliary().event() != 36104916:
+    if event.eventAuxiliary().event() != 52570562:
         continue
 
     print "run %6d, lumi %4d, event %12d" % (event.eventAuxiliary().run(), event.eventAuxiliary().luminosityBlock(),event.eventAuxiliary().event())
     
     event.getByLabel(electronLabel, electrons)
+    event.getByLabel(muonLabel, muons)
 
     event.getByLabel(lheinfoLabel,lheinfo)
 
@@ -38,6 +40,14 @@ for event in events:
     for i,el in enumerate(electrons.product()):
         if el.pt() < 5: continue
         print "elec %2d: pt %4.1f, phi %+5.3f, supercluster eta %+5.3f, sigmaIetaIeta %.3f (%.3f with full5x5 shower shapes), lost hits %d, pass conv veto %d" % (i, el.pt(), el.phi(), el.superCluster().eta(), el.sigmaIetaIeta(), el.full5x5_sigmaIetaIeta(),el.gsfTrack().hitPattern().numberOfLostHits(ROOT.reco.HitPattern.MISSING_INNER_HITS), el.passConversionVeto())
+
+    print "muons\n"
+
+    for muon in muons.product():
+        print muon.pt()
+        print muon.eta()
+        print muon.phi()
+
 
     print "lhe\n"
 
