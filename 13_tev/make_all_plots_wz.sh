dd588557524904096210e6862e2a0cf90ed4c499
Andrew <anlevin@cern.ch>
2017-04-29 01:00:36 +0200

    no commit message

---
+++ 13_tev/make_all_plots_wz.sh
@@ -0,0 +1,10 @@
+python2.6 plot_histograms_sm_observed_2016.py --lumi 35.9 -i histograms_2016_wz.root --xaxislabel "m_{jj} (GeV)" -o /afs/cern.ch/user/a/anlevin/www/tmp/mjj_observed.png --variable mjj
+
+python2.6 plot_histograms_sm_observed_2016.py --lumi 35.9 -i histograms_2016_wz.root --xaxislabel "m_{jj} (GeV)" -o /afs/cern.ch/user/a/anlevin/www/tmp/mjj_eee_observed.png --variable mjj_eee
+
+python2.6 plot_histograms_sm_observed_2016.py --lumi 35.9 -i histograms_2016_wz.root --xaxislabel "m_{jj} (GeV)" -o /afs/cern.ch/user/a/anlevin/www/tmp/mjj_eem_observed.png --variable mjj_eem
+
+python2.6 plot_histograms_sm_observed_2016.py --lumi 35.9 -i histograms_2016_wz.root --xaxislabel "m_{jj} (GeV)" -o /afs/cern.ch/user/a/anlevin/www/tmp/mjj_emm_observed.png --variable mjj_emm
+
+python2.6 plot_histograms_sm_observed_2016.py --lumi 35.9 -i histograms_2016_wz.root --xaxislabel "m_{jj} (GeV)" -o /afs/cern.ch/user/a/anlevin/www/tmp/mjj_mmm_observed.png --variable mjj_mmm
+
